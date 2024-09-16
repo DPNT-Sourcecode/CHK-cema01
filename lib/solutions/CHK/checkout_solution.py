22365ff0258801edc1fff87c6b34cc5605ef53e5
sbx_user1051 <sbx_user1051@169.254.58.181>
2024-09-16 15:09:51 +0000

    Mon Sep 16 15:09:51 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,7 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    """The checkout function calculates the total price of items in a supermarket basket based on the provided input string. The string can contain Stock Keeping Units (SKUs), each identified by a letter (e.g., A, B, C, etc.), and optionally prefixed by a number indicating the quantity of that item. If a number is not specified before a letter, a quantity of one is assumed."""
+    
+
